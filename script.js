79ab1a18933b20d162a2459504fd73ec93b562ea
RobertoRamos <robram91@gmail.com>
2020-07-08 23:38:23 -0400

    javascript functionality

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let colorLens = document.querySelectorAll(".quadrant");
+const colorLens = document.querySelectorAll(".quadrant");
 
-let start = document.querySelector;
-let sequencePattern = [];
+// const topLeft = document.querySelector(".top-left-quadrant");
+// const topRight = document.querySelector(".top-right-quadrant");
+// const bottomLeft = document.querySelector(".bottom-left-quadrant");
+// const bottomRight = document.querySelector(".bottom-right-quadrant");
+console.log(colorLens);
+const start = document.querySelector(".start");
 
-for (let i = 0; i < colorLens.length; i++) {
-  sequencePattern.push([i]);
-}
+// let sequencePattern = [];
 
-console.log(sequencePattern);
+// for (let i = 0; i < colorLens.length; i++) {
+//   sequencePattern.push(colorLens[i]);
+// }
 
-// reset form event? for restarting game?
+// console.log(sequencePattern);
 
-console.log(sequencePattern[2]);
+start.addEventListener("click", (e) => {
+  console.log("Game Started");
+  console.log(e);
+});
