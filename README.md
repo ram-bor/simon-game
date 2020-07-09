6c579d38a9f47337abf4f1927d89ec7c04f73033
RobertoRamos <robram91@gmail.com>
2020-07-08 20:17:40 -0400

    progress

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simon-game
 I want to play a game...
+
+
+Create color panels/buttons on HTML then style them in CSS
+
+Create START button to initialize game; maybe QUIT/PAUSE button for ending if you have time
+
+Create CENTER CIRCLE to hold start/pause buttons and make it look authentic and as close to the real 80s toy. 
---
+++ script.js
@@ -0,0 +1,14 @@
+let colorLens = document.querySelectorAll(".quadrant");
+
+let start = document.querySelector;
+let sequencePattern = [];
+
+for (let i = 0; i < colorLens.length; i++) {
+  sequencePattern.push([i]);
+}
+
+console.log(sequencePattern);
+
+// reset form event? for restarting game?
+
+console.log(sequencePattern[2]);
