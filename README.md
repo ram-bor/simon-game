cc4f98121d636e227aed948e0b806029f173d855
RobertoRamos <robram91@gmail.com>
2020-07-10 15:59:43 -0400

    simon says

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # simon-game
-I want to play a game...
 
+- HTML x CSS
+- Javascript x Game Flow
+
+1. HTML and CSS:
 
 Create color panels/buttons on HTML then style them in CSS
 
 Create START button to initialize game; maybe QUIT/PAUSE button for ending if you have time
 
-Create CENTER CIRCLE to hold start/pause buttons and make it look authentic and as close to the real 80s toy. 
+Create CENTER CIRCLE to hold start/pause buttons and make it look authentic and as close to the real 80s toy.
+
+Once basic style and format is completed, move on to the functionality of game.
+
+2. JAVASCRIPT:
+
+First step is to create variables (game pieces = quadrants and a sequence pattern to determine flow of game)
+
+The sequence should become randomized so that the player may follow the pattern with every correct click.
+
+GAME FLOW: Click event to initialize game by way of function (triggers sequence),
--- script.js
+++ script.js
@@ -1,21 +1,90 @@
-const colorLens = document.querySelectorAll(".quadrant");
+// declare vars
+const green = document.querySelector(".top-left-quadrant");
+const red = document.querySelector(".top-right-quadrant");
+const yellow = document.querySelector(".bottom-left-quadrant");
+const blue = document.querySelector(".bottom-right-quadrant");
 
-// const topLeft = document.querySelector(".top-left-quadrant");
-// const topRight = document.querySelector(".top-right-quadrant");
-// const bottomLeft = document.querySelector(".bottom-left-quadrant");
-// const bottomRight = document.querySelector(".bottom-right-quadrant");
-console.log(colorLens);
-const start = document.querySelector(".start");
+const quadrant = document.querySelectorAll(".quadrant");
+const startButton = document.querySelector(".start");
 
-// let sequencePattern = [];
+const allQuad = [green, red, yellow, blue];
 
-// for (let i = 0; i < colorLens.length; i++) {
-//   sequencePattern.push(colorLens[i]);
-// }
+let sequence = [];
+let intervalSequence;
+let currentSequence = 0;
+let roundCount = 0;
+let userChoice = [];
 
-// console.log(sequencePattern);
-
-start.addEventListener("click", (e) => {
-  console.log("Game Started");
+// start button
+startButton.addEventListener("click", (e) => {
+  playGame();
   console.log(e);
 });
+
+function compareInputChoice(e) {
+  let quadChoice = parseInt(e.target.dataset.index);
+  userChoice.push(quadChoice);
+  let lastElement = userChoice.length - 1;
+
+  if (
+    sequence[lastElement] == userChoice[lastElement] &&
+    userChoice.length < sequence.length
+  ) {
+    console.log("great! guess the next button");
+  } else if (
+    sequence[lastElement] == userChoice[lastElement] &&
+    userChoice.length == sequence.length
+  ) {
+    showSequence();
+  } else {
+    alert("game over!");
+  }
+}
+
+for (let i = 0; i < quadrant.length; i++) {
+  quadrant[i].addEventListener("click", compareInputChoice);
+}
+
+// add flash
+function addHighlight(button) {
+  button.classList.add("flash");
+}
+
+// remove flash
+function removeHighlight(button) {
+  button.classList.remove("flash");
+}
+
+// create flash function
+function flashQuad(button) {
+  addHighlight(button);
+  setTimeout(removeHighlight, 700, button);
+}
+
+// produce random number for sequence
+function randNumber() {
+  sequence.push(Math.floor(Math.random() * 4));
+}
+
+// iterate through each sequence round
+function iterateSequence() {
+  flashQuad(allQuad[sequence[currentSequence]]);
+  if (currentSequence == sequence.length - 1) {
+    clearInterval(intervalSequence);
+  } else {
+    currentSequence++;
+  }
+}
+
+// show player pattern sequence to player
+function showSequence() {
+  randNumber();
+  userChoice = [];
+  currentSequence = 0;
+  intervalSequence = setInterval(iterateSequence, 1000);
+}
+
+// add the play function which will initialize the game and the first sequence
+function playGame() {
+  showSequence();
+}
